ca17f988ef0085fefcb83a9cd0884b3b11317bf7
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2025-04-23 13:05:05 -0700

    some more cython

--- setup.py
+++ setup.py
@@ -4,7 +4,19 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(
-        "heatmaps/__init__.py",
+        (
+            "heatmaps/__init__.py",
+            "chmutils/__init__.py",
+            "chmengine/utils/pick.py",
+            "chmengine/utils/__init__.py",
+            "chmengine/engines/cmhmey1.py",
+            "chmengine/engines/quartney.py",
+            "chmengine/engines/cmhmey2.py",
+            "chmengine/engines/__init__.py",
+            "chmengine/play/__init__.py",
+            "chmengine/__init__.py",
+
+        ),
         compiler_directives={
             'language_level': "3",
         }
